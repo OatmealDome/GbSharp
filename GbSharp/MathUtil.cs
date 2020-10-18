6a996fedbac4dff8d62737bd4c09abb846de3720
OatmealDome <julian@oatmealdome.me>
2020-10-18 05:10:47 -0400

    MathUtil: Consolidate InRange method into one

--- GbSharp/MathUtil.cs
+++ GbSharp/MathUtil.cs
@@ -2,14 +2,9 @@ namespace GbSharp
 {
     internal class MathUtil
     {
-        public static bool InRange(ushort value, ushort start, ushort end)
-        {
-            return value >= start && value < end + 1;
-        }
-
         public static bool InRange(ushort value, ushort start, int size)
         {
-            return InRange(value, start, (ushort)(start + size));
+            return value >= start && value < start + size;
         }
 
         public static bool IsBitSet(byte b, int bit)
