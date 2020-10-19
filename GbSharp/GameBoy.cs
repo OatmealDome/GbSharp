9ceba60d3ca542074a8c54d8baa26ff25f861186
OatmealDome <julian@oatmealdome.me>
2020-10-19 16:18:38 -0400

    Project: Add basic hardware class

---
+++ GbSharp/GameBoy.cs
@@ -0,0 +1,25 @@
+﻿using GbSharp.Cpu;
+using GbSharp.Memory;
+using GbSharp.Memory.Rom;
+
+namespace GbSharp
+{
+    public class GameBoy
+    {
+        private readonly GbMemory MemoryMap;
+        private readonly GbCpu Cpu;
+
+        public GameBoy()
+        {
+            MemoryMap = new GbMemory();
+            Cpu = new GbCpu(MemoryMap);
+        }
+
+        public void LoadRom(byte[] rom)
+        {
+            SimpleRomRegion romRegion = new SimpleRomRegion(rom);
+            MemoryMap.RegisterRegion(0x0, 0x8000, romRegion);
+        }
+
+    }
+}
