8317040d237d4f27840228a89e66f9334b0d0765
OatmealDome <julian@oatmealdome.me>
2020-10-19 16:18:19 -0400

    Memory: Add SimpleRomRegion

---
+++ GbSharp/Memory/Rom/SimpleRomRegion.cs
@@ -0,0 +1,24 @@
+﻿namespace GbSharp.Memory.Rom
+{
+    class SimpleRomRegion : MemoryRegion
+    {
+        private readonly byte[] Rom;
+
+        public SimpleRomRegion(byte[] rom)
+        {
+            Rom = rom;
+        }
+
+        public override byte Read(ushort offset)
+        {
+            return Rom[offset];
+        }
+
+        public override void Write(ushort offset, byte val)
+        {
+            // for simple ROMs (no mapper), writing isn't allowed
+            // something probably went very wrong
+        }
+
+    }
+}
